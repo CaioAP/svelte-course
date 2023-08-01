de9799f870bedf3b6c1e2255a9a460eea619df18
Caio Alfonso <caioap25@gmail.com>
2023-08-01 19:46:29 -0300

    Added a form validation for the username input

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -20,9 +20,7 @@
       loading = false;
     }, 500);
   }
-
   async function confirmUsername() {
-    console.log("confirming username", username);
     const batch = writeBatch(db);
     batch.set(doc(db, "usernames", username), { uid: $user?.uid });
     batch.set(doc(db, "users", $user!.uid), { 
@@ -42,6 +40,12 @@
     username = '';
     isAvailable = false;
   }
+
+  const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+  
+  $: isValid = username?.length > 2 && username.length < 16 && re.test(username);
+  $: isTouched = username.length > 0;
+  $: isTaken = isValid && !isAvailable && !loading
 </script>
 
 <AuthCheck>
@@ -53,8 +57,30 @@
       class="input w-full"
       bind:value={username}
       on:input={checkAvailability}
-    />
-    <p>Is available? {isAvailable}</p>
-    <button class="btn btn-success"> Confirm username </button>
+      class:input-error={(!isValid && isTouched)}
+      class:input-warning={isTaken}
+      class:input-success={isAvailable && isValid && !loading}
+    />    
+    <div class="my-4 min-h-16 px-8 w-full">
+      {#if loading}
+        <p class="text-secondary">Checking availability of @{username}...</p>
+      {/if}
+
+      {#if !isValid && isTouched}
+        <p class="text-error text-sm">
+          must be 3-16 characters long, alphanumeric only
+        </p>
+      {/if}
+  
+      {#if isValid && !isAvailable && !loading}
+        <p class="text-warning text-sm">
+          @{username} is not available
+        </p>
+      {/if}
+  
+      {#if isValid && isAvailable}
+        <button class="btn btn-success">Confirm username</button>
+      {/if}
+    </div>
   </form>
 </AuthCheck>
