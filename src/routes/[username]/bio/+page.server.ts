b59a646bf27200632eddbea56bdb30f28ff60fc2
Caio Alfonso <caioap25@gmail.com>
2023-08-05 12:39:45 -0300

    Added a form to edit the bio and an action on the page server

--- src/routes/[username]/bio/+page.server.ts
+++ src/routes/[username]/bio/+page.server.ts
@@ -1,6 +1,6 @@
 import type { PageServerLoad } from "./$types";
 import { adminAuth, adminDB } from "$lib/server/admin";
-import { error, redirect } from "@sveltejs/kit";
+import { error, redirect, type Actions, fail } from "@sveltejs/kit";
 
 export const load = (async ({ locals, params }) => {
   const uid = locals.userID;
@@ -16,3 +16,22 @@ export const load = (async ({ locals, params }) => {
     bio,
   };
 }) satisfies PageServerLoad;
+
+export const actions = {
+  default: async ({ locals, request, params }) => {
+    const uid = locals.userID;
+    const data = await request.formData();
+    const bio = data.get('bio');
+    const userRef = adminDB.collection("users").doc(uid!);
+    const { username } = (await userRef.get()).data()!;
+    if (params.username !== username) {
+      throw error(401, "That username does not belong to you");
+    }
+    if (bio!.length > 260) {
+      return fail(400, { problem: "Bio must be less than 260 characters" });
+    }
+    await userRef.update({
+      bio,
+    });
+  },
+} satisfies Actions;
